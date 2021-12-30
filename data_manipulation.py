a8f829ea3324f3d21a119b049b1b1843a3dbc315
ajhinthorne <adam.j.hinthorne@gmail.com>
2021-12-29 17:50:13 -0800

    Starting the winners dataset and Project Plan

--- data_manipulation.py
+++ data_manipulation.py
@@ -1,6 +1,30 @@
-####
+####Importing Datasets
 # %%
 import pandas as pd
 import math as m
 import numpy as np
-import matplotlib.plt as plt
+import matplotlib.pyplot as plt
+
+
+###Importing CSV from Github
+# %%
+comp_by_round = pd.read_csv("https://raw.githubusercontent.com/ajhinthorne/CutThroatKitchen/master/Cutthroat%20Kitchen%20Data%20-%20DataOutput.csv")
+comp_by_round.head(5)
+
+
+####Plan for data manipulation
+##Need a dataset for sabotages to analyze sabotage type
+##Need a winners dataset to analyze winners
+
+
+### Generating the winners dataset
+##one record per winner, 
+# %%
+winner_ids = comp_by_round[(comp_by_round.winner_binary == True)]['competitor_id']
+winner_by_round = comp_by_round[(comp_by_round['competitor_id'].isin(winner_ids))]
+
+
+winner_by_round.shape
+
+
+# %%
